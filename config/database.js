a51bdff8a04015e59d5f94ca9488ae610a7dd586
Corentin Giraud <corentin.giraud@insa-lyon.fr>
2017-07-18 17:42:18 +0200

    intial commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const env = process.env;
-const username = env.MONGOD_USERNAME || "";
-const password = env.MONGOD_PASSWORD || "";
-const url = env.MONGOD_URL || "localhost:27017/kapp";
+const username = env.MONGODB_USERNAME || "";
+const password = env.MONGODB_PASSWORD || "";
+const url = env.MONGODB_URL || "localhost:27017/kapp";
 module.exports = {
     url : 'mongodb://'+username+':'+password+'@'+url,
 };
